741e9626e59dc9cd486bcc62c7919f685c5256d8
Matt Allan <mattallan@fastmail.com>
2019-05-03 15:14:23 -0400

    Add an IDE helper for the mixins

---
+++ app/Carbon/_ide_helper.php
@@ -0,0 +1,14 @@
+<?php
+namespace  {
+    exit("This file should not be included, only analyzed by your IDE");
+}
+namespace Carbon {
+    class CarbonImmutable {
+        public function presentDate(): string{}
+        public function presentTime(): string{}
+        public function presentDateTime(): string{}
+    }
+    class CarbonInterval {
+        public function presentInterval(): string{}
+    }
+}
