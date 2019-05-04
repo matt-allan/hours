3e8fccb615ab596d76913ec045be906a504dfa93
Matt Allan <mattallan@fastmail.com>
2019-05-04 12:05:30 -0400

    merge ide helpers

--- _ide_helper.php
+++ _ide_helper.php
@@ -6,6 +6,23 @@ namespace  {
     exit('This file should not be included, only analyzed by your IDE');
 }
 
+namespace Carbon {
+    interface CarbonInterface
+    {
+        public function presentDate(): string;
+
+        public function presentTime(): string;
+
+        public function presentDateTime(): string;
+    }
+    class CarbonInterval
+    {
+        public function presentInterval(): string
+        {
+        }
+    }
+}
+
 namespace Illuminate\Support {
     class Collection {
         public function toCsv(): string{}
